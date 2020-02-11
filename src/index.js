be2de8f2ecdd240d078ed5ebbd3669d67423d941
eridho.rollian <eridho.rollian@e-consult.de>
2020-02-11 16:38:40 +0100

    delete unused components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import AppShowImage from "./components";
 import * as serviceWorker from "./serviceWorker";
 
